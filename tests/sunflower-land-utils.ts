3f7a216b57bbd4e50726e6e586e1c6b04534a403
codingsh <codingsh@pm.me>
2023-05-16 18:21:30 +0000

    Initial commit

---
+++ tests/sunflower-land-utils.ts
@@ -0,0 +1,158 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  LandCreated,
+  OwnershipTransferred,
+  Paused,
+  Transfer,
+  Unpaused
+} from "../generated/SunflowerLand/SunflowerLand"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createLandCreatedEvent(
+  owner: Address,
+  landAddress: Address,
+  tokenId: BigInt
+): LandCreated {
+  let landCreatedEvent = changetype<LandCreated>(newMockEvent())
+
+  landCreatedEvent.parameters = new Array()
+
+  landCreatedEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  landCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "landAddress",
+      ethereum.Value.fromAddress(landAddress)
+    )
+  )
+  landCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return landCreatedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createPausedEvent(account: Address): Paused {
+  let pausedEvent = changetype<Paused>(newMockEvent())
+
+  pausedEvent.parameters = new Array()
+
+  pausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return pausedEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
+
+export function createUnpausedEvent(account: Address): Unpaused {
+  let unpausedEvent = changetype<Unpaused>(newMockEvent())
+
+  unpausedEvent.parameters = new Array()
+
+  unpausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return unpausedEvent
+}
